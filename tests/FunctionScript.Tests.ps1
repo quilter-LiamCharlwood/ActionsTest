ab82852c6e90186912a331cafccafb895a597b12
quilter-LiamCharlwood <liam.charlwood@quilter.com>
2023-08-31 13:38:26 +0100

    Tests added

--- tests/FunctionScript.Tests.ps1
+++ tests/FunctionScript.Tests.ps1
@@ -1,10 +1,42 @@
 BeforeAll {
     # Get-Emoji.Tests.ps1 - .Tests.ps1 + .ps1 = Get-Emoji.ps1
-    . $PSCommandPath.Replace('.Tests.ps1','.ps1')
+    .\Functions\FunctionScript.ps1
 }
 
-Describe "Get-QcredsForServer" {
-    It "Returns Get-QCred AFSUKDMN01" {
-        Get-QcredsForServer -svr "AFSUKDMN01\Server" | Should -Be (Get-QCred AFSUKDMN01)
+#mock .cred files taht get-qcred will return.
+Describe "Test Set-QcredsForServer return values" {
+    Context "Used for setting credentials based on server name." {
+        BeforeAll {
+            Mock Get-QCred -MockWith {
+                [PSCustomObject]@{
+                    UserName = "AFSUKDMN01\Bacon"
+                    Password = "*****************"
+                }
+            }
+        }
+        It "Returns that the credential is set" {
+            Set-QCredsForServer -svr "Server.skandia.co.uk" | Should -BeTrue
+        }
+        It "Returns the credentials that are the set correctly" {
+            $Creds = Set-QcredsForServer -svr "Server.skandia.co.uk" 
+            $Creds.UserName | Should -Be (Get-QCred AFSUKDMN01).UserName
+        }
+    }
+    Context "do the thing again"{
+    BeforeAll {
+        Mock Get-QCred -MockWith {
+            [PSCustomObject]@{
+                UserName = "core.local\Waffles"
+                Password = "*****************"
+            }
+        }
+    }
+        It "Returns that the credential is set" {
+            Set-QCredsForServer -svr "Server.core.local" | Should -BeTrue
+        }
+        It "Returns the credentials that are the set correctly" {
+            $Creds = Set-QcredsForServer -svr "Server.core.local" 
+            $Creds.UserName | Should -Be (Get-QCred core.local).UserName
+        }
     }
 }
